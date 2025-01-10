0b77d031ebe338bd35df04d3aa821a72084d065a
Franco Quezada <quezadafsts@gmail.com>
2025-01-10 08:46:07 -0500

    Crud semana 9

--- proyectoClase/back/controllers/teacher.controller.js
+++ proyectoClase/back/controllers/teacher.controller.js
@@ -1,10 +1,28 @@
 const modelTeacher = require('../models/teacher.models');
 
-const getList = async ()=>{
-    const result = await modelTeacher.fildAll();
+const getList = async () => {
+    const result = await modelTeacher.findAll();
     return result;
-} ;
+};
+
+const postCreate = async (data) => {
+    const result = await modelTeacher.create(data);
+    return result;
+};
+
+const postUpdate = async (data) => { 
+    const result = await modelTeacher.update(data, { where: { id: data.id } });
+    return result;
+};
+
+const postDelete = async (data) => {
+    const result = await modelTeacher.destroy({ where: { id: data.id } });
+    return result;
+};
 
 module.exports = {
-getList,
+    getList,
+    postCreate,
+    postUpdate,
+    postDelete,
 };
--- proyectoClase/back/routes/teacher.routers.js
+++ proyectoClase/back/routes/teacher.routers.js
@@ -1,19 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-const teacherController= require("../controllers/teacher.controller")
-
-
+const teacherController = require("../controllers/teacher.controller");
 
 /* Post home page. */
-router.get('/list', function (response) {
-  const result = teacherController.getList();
+router.get('/list', async function (request, response) {
+  const result = await teacherController.getList();
   response.status(200).json({
     data: result,
     status: true,
-    message: " Todo esta OK",
+    message: "Todo esta OK",
+  });
+});
+
+router.post("/create", async function (request, response) {
+  console.log(request.body);
+  const result = await teacherController.postCreate(request.body);
+
+  response.status(200).json({
+    status: true,
+    info: result,
+  });
+});
+
+router.post("/update", async function (request, response) {
+  const result = await teacherController.postUpdate(request.body);
+  response.status(200).json({
+    status: true,
+    info: result,
+  });
+});
+
+router.post("/delete", async function (request, response) {
+  console.log(request.body);
+  const result = await teacherController.postDelete(request.body);
+  response.status(200).json({
+    status: true,
+    info: result,
   });
 });
 
 module.exports = router;
- 
